7554132fb4bc0abc1e4205ae2a8b2ffeaf0f9e5a
i447092 <i447092@fhict.nl>
2021-07-22 07:31:36 +0200

    Basic display of product and category info
    routes:
    /categories (shows all diffrent categories)
    
    /category/{id} (shows products in this category)
    
    /products (shows all categories and their products)
    
    /product/{id} (shows all products and categories )

--- project/exampleProject/app/Http/Controllers/CategoryController.php
+++ project/exampleProject/app/Http/Controllers/CategoryController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Category;
-use App\Models\Product;
+use App\Category;
+use App\Product;
 
 
 class CategoryController extends Controller
 {
     public function Categories() {
-        return view('category.categories', ['categories' => Category::findAll()]);
+        return view('category.categories', ['categories' => Category::all()]);
     }
 
     public function Category($id) {
-        return view('category.category', ['category' => Category::findOrFail($id)]);
+
+        // tries to fetch the category
+        $category = Category::findOrFail($id);
+
+        // gets all the products linked to the category
+        $products = Product::where('category_id', $category->id)
+        ->orderBy('id')
+        ->get();
+
+        return view('category.category',
+        [
+            'category' => $category,
+            'products' => $products
+        ]);
     }
 }
--- project/exampleProject/app/Http/Controllers/ProductController.php
+++ project/exampleProject/app/Http/Controllers/ProductController.php
@@ -9,10 +9,35 @@ use App\Product;
 class ProductController extends Controller
 {
     public function Products() {
-        return view('product.products', ['products' => Product::findAll()]);
+        $categories = Category::all();
+
+        foreach($categories as $category) {
+            $category->products = Product::where("category_id", $category->id)
+            ->orderBy('id')
+            ->get();
+        }
+
+        return view('product.products',
+        [
+            'categories' => $categories,
+        ]);
     }
 
     public function Product($id) {
-        return view('product.product', ['product' => Product::findOrFail($id)]);
+
+        // tries to fetch the category
+        $product = Product::findOrFail($id);
+
+        // gets all the products linked to the category
+        $similar_products = Product::where('category_id', $product->category_id)
+        ->where("id", "!=", $product->id)
+        ->orderBy('id')
+        ->get();
+
+        return view('product.product',
+        [
+            'product' => $product,
+            'similar_products' => $similar_products
+        ]);
     }
 }
