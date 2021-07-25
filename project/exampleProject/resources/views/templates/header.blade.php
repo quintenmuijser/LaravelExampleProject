cd8014c22b8a220a0a7e7a8abeb5f7ff64e4b953
quintenmuijser <the_mc_pker@outlook.com>
2021-07-25 17:49:21 +0200

    Update header.blade.php

--- project/exampleProject/resources/views/templates/header.blade.php
+++ project/exampleProject/resources/views/templates/header.blade.php
@@ -1,5 +1,23 @@
-<header style="background-color: lightgrey">
-    <div class="container" style="min-height: 50px;">
-        <h4>Header content</h4>
+<header style="background-color: lightgrey" >
+    <div class="container">
+        <div class="row">
+
+            <div style="border: black solid 1px; min-height:50px;" class="col-lg-2 col-md-2 col-sm-2">
+                Logo
+            </div>
+
+            <div style="border: black solid 1px; min-height:50px;" class="col-lg-8 col-lg-8 col-sm-8">
+                Nav
+            </div>
+
+            <div style="border: black solid 1px; min-height:50px;" class="col-lg-1 col-md-1 col-sm-1">
+                Login btn
+            </div>
+
+            <div style="border: black solid 1px; min-height:50px;" class="col-lg-1 col-md-1 col-sm-1">
+                <a href="/shoppingCart">Items in cart: 0</a>
+            </div>
+
+        </div>
     </div>
 </header>
